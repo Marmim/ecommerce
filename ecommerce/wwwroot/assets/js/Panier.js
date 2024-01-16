087b036266c830d7121566ac1c9aec0459a1d8d7
Lenovo <Lenovo@MARYAME-EL0MARI>
2024-01-16 14:55:10 +0100

    Ajoutez des fichiers projet.

---
+++ ecommerce/wwwroot/assets/js/Panier.js
@@ -0,0 +1,10 @@
+﻿let cartCount = 0;
+
+function addNotif() {
+
+    // Supposons que chaque clic sur "Ajouter au panier" ajoute un produit
+    cartCount++;
+
+    // Mettez à jour le nombre dans l'icône du panier
+     cartCount=document.getElementById('cart-count').innerText ;
+}
